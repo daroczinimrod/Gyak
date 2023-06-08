fcba72effad5768b11f4c527aa701d34792a0f0a
rodi <72067090+daroczinimrod@users.noreply.github.com>
2023-06-08 23:50:44 +0200

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqgyak
+{
+    class fuvar
+    {
+        int azon;
+        string indulas;
+        int idotartam;
+        double tavolsag;
+        double dij;
+        double borravalo;
+        string fizetes;
+
+        public fuvar(int azon, string indulas, int idotartam, double tavolsag, double dij, double borravalo, string fizetes)
+        {
+            this.azon = azon;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tavolsag = tavolsag;
+            this.dij = dij;
+            this.borravalo = borravalo;
+            this.fizetes = fizetes;
+        }
+
+        public int Azon { get => azon; }
+        public string Indulas { get => indulas; }
+        public int Idotartam { get => idotartam; }
+        public double Tavolsag { get => tavolsag; }
+        public double Dij { get => dij; }
+        public double Borravalo { get => borravalo; }
+        public string Fizetes { get => fizetes; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using linqgyak;
+
+namespace linqgyak
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. feladat: Kesz");
+            List<fuvar> fuvarlista = new List<fuvar>();
+            StreamReader sr = new StreamReader("fuvar.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] adat = sr.ReadLine().Split(';');
+                fuvarlista.Add(new fuvar(int.Parse(adat[0]), adat[1], int.Parse(adat[2]), double.Parse(adat[3]), double.Parse(adat[4]), double.Parse(adat[5]), adat[6]));
+            }
+            sr.Close();
+
+            Console.WriteLine($"3.feladat: ", fuvarlista.Count());
+
+            Console.WriteLine($"4. feladat:{(fuvarlista.Count(a => a.Azon == 6185))} fuvar alatt: {fuvarlista.Where(a => a.Azon == 6185).Sum(x => x.Dij)}$");
+
+        }
+    }
+}
